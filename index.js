192bbef8b2f2b3bc1884a64b94afdda59275c259
Tanvir Ibrahim Khan <tanvirkhan1518@gmail.com>
2022-03-11 23:36:55 +0600

    readWriteStream

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
+const fs = require('fs');
 const http = require('http');
 
-const server = http.createServer((req, res) => {
-    if (req.url === '/') {
-        res.write('Hello World');
-        res.end();
-    } else if (req.url === '/about') {
-        res.write('About Page');
-        res.end();
-    } else {
-        res.write('404 Page Not Found');
-        res.end();
-    }
+const ourReadStream = fs.createReadStream(`${__dirname}/readMe.txt`, 'utf8');
+const ourWriteStream = fs.createWriteStream(`${__dirname}/writeMe.txt`);
+ourReadStream.on('data', (chunk) => {
+    ourWriteStream.write(chunk);
 });
-
-server.listen(5000);
-console.log('Server running on port 5000');
+const ourReadStream2 = fs.createReadStream(`${__dirname}/readMe2.txt`, 'utf8');
+const ourWriteStream2 = fs.createWriteStream(`${__dirname}/writeMe2.txt`);
+ourReadStream2.pipe(ourWriteStream2);
+const server = http.createServer((req, res) => {
+    const ourReadStreamServer = fs.createReadStream(`${__dirname}/readMe.txt`, 'utf8');
+    ourReadStreamServer.pipe(res);
+});
+server.listen(3000);
