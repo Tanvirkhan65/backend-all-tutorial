4ec32a8ae3f49de1afb2f6cad187937f48af4cf5
Tanvir Ibrahim Khan <tanvirkhan1518@gmail.com>
2022-03-11 17:09:02 +0600

    http module

--- index.js
+++ index.js
@@ -1,49 +1,17 @@
-/* eslint-disable no-shadow */
-const path = require('path');
-const os = require('os');
-const fs = require('fs');
-const events = require('events');
-const School = require('./school');
+const http = require('http');
 
-const emitter = new events.EventEmitter();
-const myPath = ' E:/nodejs_backend/index.js';
-console.log(path.basename(myPath));
-console.log(path.dirname(myPath));
-console.log(path.extname(myPath));
-console.log(path.parse(myPath));
-console.log(os.platform());
-console.log(os.homedir());
-console.log(os.freemem() / 1024 / 1024);
-console.log(os.cpus());
-console.log(os.type());
-fs.writeFileSync('myfile.txt', 'Hello World');
-fs.writeFileSync('myfile.txt', ' Hello World 2 ', { flag: 'a' });
-// appendFileSync and flag a is same as writeFileSync
-// flag a is appendFileSync
-
-fs.appendFileSync('myfile.txt', 'Hello World 3');
-const data = fs.readFileSync('myfile.txt', 'utf8');
-console.log(data);
-fs.writeFile('myfile.txt', 'Hello World 4', (err) => {
-    if (err) throw err;
-    console.log('File Written');
-});
-fs.readFile('myfile.txt', 'utf8', (err, data) => {
-    if (err) throw err;
-    console.log(data);
-});
-emitter.on('bellring', ({ message, time }) => {
-    console.log(`${message} at ${time}`);
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.write('Hello World');
+        res.end();
+    } else if (req.url === '/about') {
+        res.write('About Page');
+        res.end();
+    } else {
+        res.write('404 Page Not Found');
+        res.end();
+    }
 });
 
-setTimeout(() => {
-    emitter.emit('bellring', {
-        message: 'Ring Ring',
-        time: new Date().toLocaleTimeString(),
-    });
-}, [2000]);
-const school = new School();
-school.on('periodStarted', ({ message, time }) => {
-    console.log(`${message} at ${time}`);
-});
-school.startPeriod();
+server.listen(5000);
+console.log('Server running on port 5000');
