ca80ac86dc7c24b66c8683e7d564a6e1aae24323
Tanvir Ibrahim Khan <tanvirkhan1518@gmail.com>
2022-03-12 21:53:16 +0600

    requestResponse

--- index.js
+++ index.js
@@ -1,18 +1,46 @@
-const mathLibrary = require('./lib/math');
-const quotesLibrary = require('./lib/quotes');
-
+/* Title:Uptime monitoring system
+description:This is a simple uptime monitoring system.
+*/
+const http = require('http');
+const url = require('url');
+const { StringDecoder } = require('string_decoder');
+// app scaffolding
 const app = {};
+// app configuration
 app.config = {
-    timeBetweenQuotes: 1000,
+    port: 3000,
 };
-app.printAQuote = () => {
-    const allQuotes = quotesLibrary.allQuotes();
-    const numberOfQuotes = allQuotes.length;
-    const randomNumber = mathLibrary.getRandomNumber(1, numberOfQuotes);
-    const selectedQuote = allQuotes[randomNumber - 1];
-    console.log(selectedQuote);
+// app create server
+app.createServer = () => {
+    const server = http.createServer(app.handleReqRes);
+    server.listen(app.config.port, () => {
+        console.log(`Server running at http://localhost:${app.config.port}`);
+    });
 };
-app.indefiniteLoop = () => {
-    setInterval(app.printAQuote, app.config.timeBetweenQuotes);
+app.handleReqRes = (req, res) => {
+    // request handling
+    // get the parseURL
+    const parseURL = url.parse(req.url, true);
+    const path = parseURL.pathname;
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+    const method = req.method.toLowerCase();
+    const queryStringObject = parseURL.query;
+    const { headers } = req;
+    const decoder = new StringDecoder('utf-8');
+    let realData = '';
+    res.on('data', (buffer) => {
+        realData += decoder.write(buffer);
+    });
+    req.on('end', () => {
+        realData += decoder.end();
+        console.log(JSON.parse(realData));
+        res.end('Hello World!');
+    });
+    console.log(parseURL);
+    console.log(path);
+    console.log(trimmedPath);
+    console.log(method);
+    console.log(queryStringObject);
+    console.log(headers);
 };
-app.indefiniteLoop();
+app.createServer();
